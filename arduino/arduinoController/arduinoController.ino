67b5cfe42149da57f065e2a0b67570e632af7bf0
ChaelPix <chael.pixel@gmail.com>
2023-08-22 16:39:40 +0200

    arduino

--- arduino/arduinoController/arduinoController.ino
+++ arduino/arduinoController/arduinoController.ino
@@ -1,59 +1,103 @@
 #include <ros.h>
-#include <std_msgs/Float64.h>
+#include <std_msgs/Float32.h>
 #include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
 
 #include <Arduino.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #include <MeMegaPi.h>
 
-//motors
-MeMegaPiDCMotor m_moteurG(PORT1B);
-MeMegaPiDCMotor m_moteurD(PORT2B);
+#define SERIAL_PORT "/dev/ttyAMA0"
 
-//ultra sensor
-MeUltrasonicSensor m_ultraSensor(PORT_5);
-bool isrunning = false;
 
+//-------Components
+MeMegaPiDCMotor m_moteurG(PORT2B);
+MeMegaPiDCMotor m_moteurD(PORT4B);
+
+
+//-------Ros Node
 ros::NodeHandle nh;
 
-//Pub
-std_msgs::Float64 str_f;
-ros::Publisher pub("foo", &str_f);
+//-------PUBs
+std_msgs::Bool callback_msg;
+ros::Publisher pubMotorsCallBack("/MotorsCallBack", &callback_msg);
 
-void messageCb(const std_msgs::Float64& msg)
+
+//-----Motors
+void MoveForward(const std_msgs::Float32& msg)
 {
-  Serial.println("message recu !");
   int speed = static_cast<int>(msg.data);
-   m_moteurG.run(speed);
-   m_moteurD.run(-speed);
-   isrunning = true;
-   //pub.publish(&msg);
+  m_moteurG.run(-speed);
+  m_moteurD.run(speed);
+  SendMotorsOk();
+}
+void MoveBackward(const std_msgs::Float32& msg)
+{
+  int speed = static_cast<int>(msg.data);
+  m_moteurG.run(speed);
+  m_moteurD.run(-speed);
+  SendMotorsOk();
+}
+void TurnLeft(const std_msgs::Float32& msg)
+{
+  int speed = static_cast<int>(msg.data);
+  m_moteurG.run(-speed);
+  m_moteurD.run(-speed);
+  SendMotorsOk();
+}
+void TurnRight(const std_msgs::Float32& msg)
+{
+  int speed = static_cast<int>(msg.data);
+  m_moteurG.run(speed);
+  m_moteurD.run(speed);
+  SendMotorsOk();
+}
+void StopMotors()
+{
+  m_moteurG.stop();
+  m_moteurD.stop();
+  SendMotorsOk();
+}
+void SendMotorsOk()
+{
+  callback_msg.data = true;
+  pubMotorsCallBack.publish(&callback_msg);
+}
+//----STOP
+void stopRos(const std_msgs::Float32& msg)
+{
+  StopMotors();
 }
 
-//Sub
-ros::Subscriber<std_msgs::Float64> sub("superFloat", &messageCb);
+//--------SUBs
+ros::Subscriber<std_msgs::Float32> subMotorsMoveForward("motorsMoveForward", &MoveForward);
+ros::Subscriber<std_msgs::Float32> subMotorsMoveBackward("motorsMoveBackward", &MoveBackward);
+ros::Subscriber<std_msgs::Float32> subMotorsTurnLeft("motorsTurnLeft", &TurnLeft);
+ros::Subscriber<std_msgs::Float32> subMotorsTurnRight("motorsTurnRight", &TurnRight);
+ros::Subscriber<std_msgs::Empty> subMotorsStop("motorsStop", &StopMotors);
+ros::Subscriber<std_msgs::Float32> subStop("stopRos", &stopRos);
 
+//-----------Setup
 void setup()
 {
-  
-    nh.initNode();
-    nh.advertise(pub);
-    nh.subscribe(sub);
+  nh.initNode();
+
+  //Subs
+  nh.subscribe(subMotorsMoveForward);
+  nh.subscribe(subMotorsMoveBackward);
+  nh.subscribe(subMotorsTurnLeft);
+  nh.subscribe(subMotorsTurnRight);
+  nh.subscribe(subMotorsStop);
+  nh.subscribe(subStop);
+
+  //Pubs
+  nh.advertise(pubMotorsCallBack);
 }
 
 void loop()
 {
   nh.spinOnce();
-
-  if(isrunning)
-  {
-      if(m_ultraSensor.distanceCm() < 5)
-      {
-        isrunning = false;
-         m_moteurG.run(0);
-         m_moteurD.run(0);
-      }
-  }
-    delay(1);
+  delay(1);
 }
