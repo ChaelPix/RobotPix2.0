df99e328eff883ac3a025c6858dfcb4b5e8da913
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-09-11 13:52:36 +0200

    Add files via upload

--- arduino/arduinoController/arduinoController.ino
+++ arduino/arduinoController/arduinoController.ino
@@ -2,6 +2,7 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
 #include <geometry_msgs/Vector3.h>
 
 #include <Arduino.h>
@@ -9,6 +10,8 @@
 #include <SoftwareSerial.h>
 #include <MeMegaPi.h>
 
+// MeEncoderOnBoard Encoder_1(SLOT2);
+// MeEncoderOnBoard Encoder_2(SLOT4);
 //-------Variables
 bool _isStopped = false;
 //-------Ros Node
@@ -18,8 +21,11 @@ ros::NodeHandle nh;
 std_msgs::Bool callback_msg;
 ros::Publisher pubMotorsCallBack("/MotorsCallBack", &callback_msg);
 ros::Publisher pubRotateCallBack("/RotateCallBack", &callback_msg);
+ros::Publisher pubStampCallBack("/stampCallback", &callback_msg);
+std_msgs::String lcd_msg;
+ros::Publisher pubLcdText("/setLcdText", &lcd_msg);
 
-//-----Motors - x: speed, y: 0 = Stop, 1 = Forward, 2 = Backward, 3 = Left, 4 = Right
+//-----Motors - x: speed, y: 0 = Stop, 1 = Forward, 2 = Backward, 3 = Left, 4 = Right, 5 = RoateSet, 6 = StampOn, 7 = StampOff
 void GetMotorsControl(const geometry_msgs::Vector3& msg)
 {
   int speed = static_cast<int>(msg.x); 
@@ -38,8 +44,20 @@ void GetMotorsControl(const geometry_msgs::Vector3& msg)
       TurnRight(speed); break;
     case 5:
       AngleRotate_Set(speed, static_cast<float>(msg.z)); break;
+    case 6:
+      StampOn(speed); break;
+    case 7:
+      StampOff(speed); break;
+    case 8:
+      StampObject(speed); break;
   }
 }
+
+void InitArduino(const std_msgs::Empty& msg)
+{
+  calibrateGyro();
+}
+
 //----STOP
 void stopRos(const std_msgs::Float32& msg)
 {
@@ -50,6 +68,7 @@ void stopRos(const std_msgs::Float32& msg)
 //--------SUBs
 ros::Subscriber<geometry_msgs::Vector3> subMotorsControl("motorsControl", &GetMotorsControl);
 ros::Subscriber<std_msgs::Float32> subStop("stopRos", &stopRos);
+ros::Subscriber<std_msgs::Empty> subInit("initArduino", &InitArduino);
 
 //-----------Setup
 void setup()
@@ -59,16 +78,31 @@ void setup()
   //Subs
   nh.subscribe(subMotorsControl);
   nh.subscribe(subStop);
+  nh.subscribe(subInit);
 
   //Pubs
   nh.advertise(pubMotorsCallBack);
   nh.advertise(pubRotateCallBack);
+  nh.advertise(pubLcdText);
+  nh.advertise(pubStampCallBack);
 
+  //InitializeEncoders();
   SetupGyro();
+}
+
+void _delay(float ms) {
+  if (ms < 0.0) {
+    ms = 0.0;
+  }
+  long endTime = millis() + ms;
+  while (millis() < endTime) _loop();
 }
 
 void _loop()
 {
+  // Encoder_1.loop();
+  // Encoder_2.loop();
+  
   nh.spinOnce();
   UpdateGyro();
   delay(1);
